ee275ad817eb076131c48976a9849dd8eff33cbd
DevSORA <sodaoov@gmail.com>
2023-04-14 19:43:06 +0900

    chore: webpack, babel 적용

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src')],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  mode: 'development',
+};

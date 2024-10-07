7451a53dec76b6deebcb9b3b01b5fb7820e0bdc5
developer-sora <sodaoov@gmail.com>
2024-10-07 16:34:14 +0900

    sitemap 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://developer-sora.github.io/todogram/',
+  generateRobotsTxt: true, // (optional) robots.txt 파일도 생성
+  changefreq: 'monthly',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
